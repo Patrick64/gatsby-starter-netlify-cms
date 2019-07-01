3ef693923222f043743f9a643d69620e7322a2de
Patrick Woodcock <patrick@ralphmedia.co.uk>
2019-07-01 16:24:48 +0100

    fix interview page

--- src/templates/interview.js
+++ src/templates/interview.js
@@ -1,76 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { kebabCase } from 'lodash'
-import Helmet from 'react-helmet'
+// import { kebabCase } from 'lodash'
+// import Helmet from 'react-helmet'
 import { graphql, Link } from 'gatsby'
 import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
+// import Content, { HTMLContent } from '../components/Content'
 
-export const InterviewTemplate = ({
-  // content,
-  contentComponent,
-  title,
-  // featuredimage,
-  // date,
-  // interviewerName,
-  // intervieweeName,
-  // transcription,
-  // featuredpost,
-  // subjects,
-  // description,
-  helmet,
-}) => {
-  const PostContent = contentComponent || Content
 
-  return (
-    <section className="section">
-      {helmet || ''}
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <PostContent content={transcription} />
-          </div>
-        </div>
-      </div>
-    </section>
-  )
+
+export default class Interview extends React.Component {
+  render()  {
+    return (
+      <Layout>
+        
+          <h1>{this.props.data.markdownRemark.frontmatter.title}</h1>
+        
+      </Layout>
+    ) 
+  }
 }
-
-InterviewTemplate.propTypes = {
-  contentComponent: PropTypes.func,
-  title: PropTypes.string,
-  helmet: PropTypes.object,
-}
-
-const Interview = ({ data }) => {
-  const { markdownRemark: post } = data
-
-  return (
-    <Layout>
-      <InterviewTemplate
-        content={post.html}
-        contentComponent={HTMLContent}
-        helmet={
-          <Helmet titleTemplate="%s | Interview">
-            <title>{`${post.frontmatter.title}`}</title>
-          </Helmet>
-        }
-        title={post.frontmatter.title}
-      />
-    </Layout>
-  )
-}
-
-Interview.propTypes = {
-  data: PropTypes.shape({
-    markdownRemark: PropTypes.object,
-  }),
-}
-
-export default Interview
 
 export const pageQuery = graphql`
   query InterviewByID($id: String!) {
