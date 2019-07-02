6b3c4caa253ca3ff0b76acbb2c34d92e9ed3833c
Patrick Woodcock <patrick@ralphmedia.co.uk>
2019-07-02 10:39:42 +0100

    more interview stuff

--- src/templates/interview.js
+++ src/templates/interview.js
@@ -1,22 +1,32 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 // import { kebabCase } from 'lodash'
 // import Helmet from 'react-helmet'
-import { graphql, Link } from 'gatsby'
-import Layout from '../components/Layout'
+import { graphql, Link } from "gatsby";
+import Layout from "../components/Layout";
 // import Content, { HTMLContent } from '../components/Content'
 
-
-
 export default class Interview extends React.Component {
-  render()  {
+  render() {
+    
     return (
       <Layout>
-        
-          <h1>{this.props.data.markdownRemark.frontmatter.title}</h1>
-        
+        <section className="section">
+          <div className="container content">
+            <div className="columns">
+              <div className="column is-10 is-offset-1">
+                <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+                  {this.props.data.markdownRemark.frontmatter.title}
+                </h1>
+                <p>Interviewer: {this.props.data.markdownRemark.frontmatter.interviewerName} </p>
+                <p>Interviewee: {this.props.data.markdownRemark.frontmatter.intervieweeName} </p>
+                <div className="content" dangerouslySetInnerHTML={{ __html: this.props.data.markdownRemark.html }} />
+              </div>
+            </div>
+          </div>
+        </section>
       </Layout>
-    ) 
+    );
   }
 }
 
@@ -28,7 +38,9 @@ export const pageQuery = graphql`
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         title
+        interviewerName
+        intervieweeName
       }
     }
   }
-`
+`;
